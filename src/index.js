395d3a56a8e348a0bd7c6dd9632ded3597ce0494
Luybov Kurij <lubovhimicka@gmail.com>
2023-09-24 17:57:22 +0300

    feat: aad solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let matrix1 = [...matrix];
+    let reverse = matrix1[1].reverse();
+    let result = matrix1.flat();
+  return result;
 }
